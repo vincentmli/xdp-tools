9ea7433d4b8ba62890da8e2ee3b8d28b48bbba14
Vincent Li <vincent.mc.li@gmail.com>
2024-09-07 22:21:09 +0000

    add xdp-dns user space test program

--- xdp-dns/xdp_dns.c
+++ xdp-dns/xdp_dns.c
@@ -1,78 +1,25 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <inttypes.h>
-
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
-#include <xdp/libxdp.h>
-#include <arpa/inet.h>
-
-#include <linux/if_ether.h>
-
-#include "params.h"
-#include "logging.h"
-#include "util.h"
-
-#include <net/if.h>
-#include <assert.h>
-#include <getopt.h>
-#include <sys/types.h>
+#include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
-#include <linux/if_link.h>
+int main() {
+    int map_fd;
+    char *domain = "www.bpfire.net";
+    __u8 value = 1;
 
-#include "xdp_dnsrrl_kern.skel.h"
+    map_fd = bpf_obj_get("/sys/fs/bpf/xdp-dns/domain_denylist");
+    if (map_fd < 0) {
+        fprintf(stderr, "Failed to open map: %s\n", strerror(errno));
+        return 1;
+    }
 
-#define DEFAULT_IPv4_VIP_PINPATH "/sys/fs/bpf/rrl_exclude_v4_prefixes"
-#define DEFAULT_IPv6_VIP_PINPATH "/sys/fs/bpf/rrl_exclude_v6_prefixes"
-#define DEFAULT_RATELIMIT 0x20
-#define DEFAULT_CPUS 0x2
+    if (bpf_map_update_elem(map_fd, domain, &value, BPF_ANY) != 0) {
+        fprintf(stderr, "Failed to update map: %s\n", strerror(errno));
+        return 1;
+    }
 
-#define EXCLv4_TBL "exclude_v4_prefixes"
-#define EXCLv6_TBL "exclude_v6_prefixes"
-
-static unsigned int ifindex;
-
-static unsigned long ratelimit = DEFAULT_RATELIMIT;
-static unsigned long cpus = DEFAULT_CPUS;
-
-static const struct loadopt {
-        bool help;
-        struct iface iface;
-        enum xdp_attach_mode mode;
-} defaults_load = {
-        .mode = XDP_MODE_NATIVE,
-};
-
-struct enum_val xdp_modes[] = {
-       {"native", XDP_MODE_NATIVE},
-       {"skb", XDP_MODE_SKB},
-       {"hw", XDP_MODE_HW},
-       {NULL, 0}
-};
-
-static struct prog_option load_options[] = {
-        DEFINE_OPTION("mode", OPT_ENUM, struct loadopt, mode,
-                      .short_opt = 'm',
-                      .typearg = xdp_modes,
-                      .metavar = "<mode>",
-                      .help = "Load XDP program in <mode>; default native"),
-        DEFINE_OPTION("dev", OPT_IFNAME, struct loadopt, iface,
-                      .positional = true,
-                      .metavar = "<ifname>",
-                      .required = true,
-                      .help = "Load on device <ifname>"),
-        END_OPTIONS
-};
-
-
-int main(int argc, char **argv)
-{
+    printf("Domain %s added to denylist\n", domain);
+    return 0;
 }
