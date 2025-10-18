12280ef22ae49f75eda047144ed3e9dc0f73e04a
Vincent Li <vincent.mc.li@gmail.com>
2025-10-17 19:45:07 -0700

    tc-basic-classifier: add user space program
    the bpf skel header is generated only if USER_TARGETS
    is added in Makefile, so add a dummy user space program.
    
    Signed-off-by: Vincent Li <vincent.mc.li@gmail.com>

---
+++ tc-basic-classifier/class_filter.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <ifaddrs.h>
+#include <linux/if_link.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <signal.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include "class_filter.skel.h"
+
+int main()
+{
+	return 0;
+}
